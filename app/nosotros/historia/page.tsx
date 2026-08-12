8af5224634db22325a9993e8b330ffe97e1d3056
AnahDev <anadevtools@gmail.com>
2026-08-11 22:45:31 -0400

    feat: Agg componente en reseña  historica

--- app/nosotros/historia/page.tsx
+++ app/nosotros/historia/page.tsx
@@ -1,5 +1,6 @@
 import About from "../../components/about/About";
 import InfoCard from "../../components/about/InfoCard";
+import HistoryFeature from "@/app/components/home/HistoryFeature";
 
 export default function Home() {
     return (
@@ -59,7 +60,47 @@ export default function Home() {
                 </div>
             </section>
 
+
+            <section className="mt-20">
+                <About
+                    badge="Algo de Historia"
+                    title="Luisa Cáceres"
+                    paragraphs={[
+                        "Luisa Cáceres Díaz de Arismendi nació en Caracas el 25 de septiembre de 1799. Su padre, Domingo Cáceres, y su hermano Félix fueron asesinados por los realistas en el pueblo de Ocumare en 1814, lo que la llevó a participar en el Éxodo de Caracas de 1814 y emigrar con el resto de su familia a Isla Margarita. Se casó con el general Juan Bautista Arismendi el 4 de diciembre de 1814; se habían conocido en Caracas durante la Navidad de 1813.",
+
+                        "En 1815, fue detenida por las autoridades españolas con el propósito de presionar a su esposo Arismendi, quien para entonces estaba librando una feroz campaña contra las fuerzas realistas.Fue llevada a Cádiz; sin embargo, en el camino, el barco en el que viajaba fue atacado por un buque corsario, y ella y otros pasajeros quedaron varados en la Isla Santa María, ubicada en las Azores. Luisa finalmente llegó a Cádiz el 17 de enero de 1817. Fue presentada al capitán general de Andalucía, quien protestó contra la decisión arbitraria de las autoridades españolas en las Américas, y le otorgó a Luisa la categoría de confinada.",
+
+                        "No obstante, nunca abandonó sus ideales independentistas. Después de ser liberada, regresó a Venezuela el 26 de julio de 1818, y continuó apoyando las ideas de libertad y soberanía del pueblo americano. Vivió en Caracas hasta su muerte el 2 de junio de 1866. En reconocimiento a su lucha por la independencia venezolana, sus restos fueron sepultados en el Panteón Nacional en 1876. Actualmente, aparece en el billete de 20 bolívares fuertes.",
+
+                    ]}
+                    imageSrc="/images/luisa-caceres-arismendi.jpg"
+                    imageAlt="Retrato de Luisa Cáceres de Arismendi"
+                    reverse={true}
+                />
+            </section>
+
+
+            {/* <div className="mt-20">
+                <HistoryFeature
+                    badge="Nuestra Historia"
+                    title="Luisa Cáceres"
+                    paragraphs={[
+                        "Luisa Cáceres Díaz de Arismendi nació en Caracas el 25 de septiembre de 1799.[1] Su padre, Domingo Cáceres, y su hermano Félix fueron asesinados por los realistas en el pueblo de Ocumare en 1814, lo que la llevó a participar en el Éxodo de Caracas de 1814 y emigrar con el resto de su familia a Isla Margarita.[2] Se casó con el general Juan Bautista Arismendi el 4 de diciembre de 1814; se habían conocido en Caracas durante la Navidad de 1813.",
+
+                        "En 1815, fue detenida por las autoridades españolas con el propósito de presionar a su esposo Arismendi, quien para entonces estaba librando una feroz campaña contra las fuerzas realistas.[3] Sin embargo, el gobernador de la isla, Joaquín Urreiztieta, no logró obtener nada de ella ni de su esposo, por lo que Luisa permaneció prisionera en la fortaleza de Santa Rosa, donde dio a luz a una niña que murió al nacer.[4] Posteriormente, fue trasladada a la fortaleza de Pampatar, luego a La Guaira y finalmente fue enviada a España en 1816, donde también fue víctima de torturas para que renunciara a sus ideales republicanos.",
+
+                        "Fue llevada a Cádiz; sin embargo, en el camino, el barco en el que viajaba fue atacado por un buque corsario, y ella y otros pasajeros quedaron varados en la Isla Santa María, ubicada en las Azores. Luisa finalmente llegó a Cádiz el 17 de enero de 1817.[3] Fue presentada al capitán general de Andalucía, quien protestó contra la decisión arbitraria de las autoridades españolas en las Américas, y le otorgó a Luisa la categoría de confinada.",
+
+                        "No obstante, nunca abandonó sus ideales independentistas. Después de ser liberada, regresó a Venezuela el 26 de julio de 1818,[3] y continuó apoyando las ideas de libertad y soberanía del pueblo americano. Vivió en Caracas hasta su muerte el 2 de junio de 1866. En reconocimiento a su lucha por la independencia venezolana, sus restos fueron sepultados en el Panteón Nacional en 1876. Actualmente, aparece en el billete de 20 bolívares fuertes.",
+                    ]}
+                    imageSrc="/images/luisa-caceres-arismendi.jpg"
+                    imageAlt="Retrato de Luisa Cáceres de Arismendi"
+                />
+            </div> */}
+
+
+
             {/* Resto de tus componentes... */}
-        </main>
+        </main >
     );
 }
